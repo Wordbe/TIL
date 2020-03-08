caec5b1602093405b4b9d790a1d29bb39980ee80
Seongho, Jin <seonghojin3@gmail.com>
2020-03-09 00:12:06 +0900

    200309 react arranged

--- web/7 Spring/spring-jdbc/dao/RoleDaoSqls.java
+++
@@ -1,9 +0,0 @@
-package kr.or.connect.daoexam.dao;
-
-public class RoleDaoSqls {
-	// 상수는 대문자, 언더바 조합의 name convention을 지킬 것
-	public static final String SELECT_ALL = "SELECT role_id, description FROM role ORDER BY role_id";
-	public static final String UPDATE = "UPDATE role set description = :description WEHRE role_id = :roleId";
-	public static final String SELECT_BY_ROLE_ID = "SELECT role_id, description FROM role WHERE role_id = :roleId";
-	public static final String DELETE_BY_ROLE_ID = "DELETE FROM role WHERE role_id = :roleId";
-}
